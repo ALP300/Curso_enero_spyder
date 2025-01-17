5a90458108f72a26fd093b263a9f82cecd7a5f15
ALP300 <aitorpalomino1@gmail.com>
2025-01-16 21:01:41 -0500

    Realizar ejercicio 1

--- ejer1.py
+++ ejer1.py
@@ -1,1 +1,10 @@
-print("Hola")
+import pandas as pd
+import numpy as np
+
+datos= {'Nombre':['Leonardo','Joaquin','Alex','Paula','Enrique', 'Jhon'],
+        'Materias':['Lenguaje','EDF','Química','Matemáticas','Inglés','Historia'],
+        'Calificaciones':['18','20','13','19','16','17'],
+        'Deportes':['Fútbol','Natación','Tenis','Voley','Natación','Balón Mano']
+        }
+df= pd.DataFrame(datos)
+print(df)
