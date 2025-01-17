f11cd4d35795623ea6660914a436a6bc44a16ebe
ALP300 <aitorpalomino1@gmail.com>
2025-01-16 21:08:37 -0500

    Actualizar

--- ejer1.py
+++ ejer1.py
@@ -8,3 +8,16 @@ datos= {'Nombre':['Leonardo','Joaquin','Alex','Paula','Enrique', 'Jhon'],
         }
 df= pd.DataFrame(datos)
 print(df)
+print('\n'*4)
+
+#DATOS NO VÁLIDOS
+datos2= {'Nombre':['Leonardo','Joaquin','N/A','Paula','Enrique', 'Jhon'],
+        'Materias':['Lenguaje','EDF','Química','Matemáticas','Inglés','N/A'],
+        'Calificaciones':['18','20',np.nan,'19','16','17'],
+        'Deportes':['Fútbol','Natación','Tenis','Voley','N/A','Balón Mano']
+        }
+df2= pd.DataFrame(datos2)
+print(df2)
+print('\n'*1)
+print(df2.info())
+
